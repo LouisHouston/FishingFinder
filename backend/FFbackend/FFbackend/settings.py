ada33f7b3ed0744357c6f853eaa14ae895596a55
LouisHouston <69780196+LouisHouston@users.noreply.github.com>
2025-05-31 23:07:37 -0700

    one of my keys were showing oops updated

--- backend/FFbackend/FFbackend/settings.py
+++ backend/FFbackend/FFbackend/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*e=p!!j7*(u0)2i!&sex@%@!dr3bq#6$e%16rt4&z2lzap%w+5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = ['*']
 
